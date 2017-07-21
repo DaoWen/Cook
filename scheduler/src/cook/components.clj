b6d290be92c556da36a52950d4decd7d4eb7ed51
Shams Imam <shams.imam@twosigma.com>
2017-07-21 09:34:32 -0400

    -- add support for gradual transition to the cook executor

--- scheduler/src/cook/components.clj
+++ scheduler/src/cook/components.clj
@@ -382,7 +382,8 @@
                                                     :default-progress-regex-string "progress: (\\d*)(?: )?(.*)"
                                                     :log-level "INFO"
                                                     :max-message-length 512
-                                                    :progress-sample-interval-ms (* 1000 60 5)}
+                                                    :progress-sample-interval-ms (* 1000 60 5)
+                                                    :transition-percent 2}
                            default-uri-config {:cache true
                                                :executable true
                                                :extract false}]
